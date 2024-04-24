13e15193a451946e0f7c8dbdecc9d96ebedae2de
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-04-24 12:18:21 +0500

    add colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        blue: "#4661e6",
+        "blue-light": "#62bcfa",
+        "blue-lighter": "#cfd7ff",
+        "blue-lightest": "#f2f4ff",
+        purple: "#ad1fea",
+        "orange-light": "#f49f85",
+        "gray-dark": "#647196",
+        gray: "#f2f4ff",
+        "gray-light": "#f7f8fd",
+        "blue-dark-1": "#4661e6",
+        "blue-dark-2": "#3a4374",
+        red: "#d73737",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["var(--font-jost)"],
       },
     },
   },
