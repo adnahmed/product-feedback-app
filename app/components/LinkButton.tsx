23a43cd8c1b257b6d7f241fc0d6333f161e92bca
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-04-26 01:10:03 +0500

    clean LinkButton from event etc.

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,4 +1,3 @@
-import { MouseEventHandler, ReactNode } from "react";
 import { tw } from "../lib/tailwindest";
 import { UpArrow } from "./UpArrow";
 
@@ -31,9 +30,7 @@ const linkButton = tw.toggle({
   },
   base: {
     display: "flex",
-    marginX: "mx-auto",
-    marginY: "my-4",
-    paddingY: "py-[8px]",
+    paddingY: "py-[4px]",
     paddingX: "px-[14px]",
     borderRadius: "rounded-lg",
     alignItems: "items-center",
@@ -46,21 +43,23 @@ const linkButton = tw.toggle({
   },
 });
 
-const linkButtonIcon = tw.style({
-  paddingTop: "pt-[8px]",
-});
-
 export function LinkButton({
   onClick,
   children,
   icon = false,
 }: {
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+  onClick?: (value: string) => void;
   icon?: boolean;
-  children: ReactNode;
+  children: string;
 }) {
   return (
-    <button className={linkButton.class(!!icon)} onClick={onClick}>
+    <button
+      className={linkButton.class(!!icon)}
+      onClick={(e) => {
+        e.preventDefault();
+        if (onClick) onClick(children);
+      }}
+    >
       {icon && <UpArrow />}
       <span>{children}</span>
     </button>
