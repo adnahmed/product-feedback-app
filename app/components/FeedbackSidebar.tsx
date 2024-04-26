7b6b1bfb88b99bb5daf5f36ef2e6fd065d755674
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-04-26 20:21:55 +0500

    add shade to feedback sidebar

--- app/components/FeedbackSidebar.tsx
+++ app/components/FeedbackSidebar.tsx
@@ -5,35 +5,82 @@ import { tw } from "../lib/tailwindest";
 import { CategorySelection } from "./CategorySelection";
 import { FeedbackAppName } from "./FeedbackAppName";
 import { RoadmapSummary } from "./RoadmapSummary";
-const sidebar = tw.style({
-  backgroundColor: "bg-gray",
-  display: "flex",
-  flexDirection: "flex-col",
-  justifyContent: "justify-center",
-  gap: "gap-[24px]",
-  "@tablet": {
-    flexDirection: "tablet:flex-row",
+const sidebar = tw.toggle({
+  truthy: {
+    display: "flex",
+    alignItems: "items-end",
+    position: "absolute",
+    zIndex: "z-[900]",
+    right: "-right-0",
+    height: "h-[100vh]",
+    width: "w-max",
+    padding: "p-[24px]",
   },
-  "@desktop": {
-    flexDirection: "desktop:flex-col",
+  falsy: {},
+  base: {
+    backgroundColor: "bg-gray",
+
+    flexDirection: "flex-col",
+    gap: "gap-[24px]",
+    display: "hidden",
+    "@tablet": {
+      alignItems: "tablet:items-start",
+      width: "tablet:w-max",
+      position: "tablet:static",
+      height: "tablet:h-max",
+      zIndex: "tablet:-z-0",
+      display: "tablet:flex",
+      padding: "tablet:p-0",
+      flexDirection: "tablet:flex-row",
+    },
+    "@desktop": {
+      display: "desktop:flex",
+      flexDirection: "desktop:flex-col",
+    },
   },
 });
-export function SidebarMenu() {
+export function SidebarMenu({ menuOpen }: { menuOpen: boolean }) {
   return (
-    <div className={sidebar.class}>
-      <CategorySelection />
-      <RoadmapSummary />
+    <div>
+      <div className={shade.class(menuOpen)}></div>
+      <div className={sidebar.class(menuOpen)}>
+        <CategorySelection />
+        <RoadmapSummary />
+      </div>
     </div>
   );
 }
 const sidebarContainer = tw.style({
+  position: "relative",
+  zIndex: "z-10",
   "@tablet": {
     display: "tablet:flex",
     alignItems: "tablet:items-center",
+    gap: "tablet:gap-[24px]",
   },
   "@desktop": {
     display: "desktop:flex",
+    gap: "desktop:gap-[24px]",
     flexDirection: "desktop:flex-col",
+  },
+});
+const shade = tw.toggle({
+  truthy: {
+    display: "flex",
+  },
+  falsy: {
+    display: "hidden",
+  },
+  base: {
+    position: "fixed",
+    width: "w-full",
+    height: "h-full",
+    backgroundColor: "bg-black",
+    opacity: "opacity-50",
+    zIndex: "z-[800]",
+    "@tablet": {
+      display: "tablet:hidden",
+    },
   },
 });
 export function FeedbackSidebar() {
@@ -41,7 +88,7 @@ export function FeedbackSidebar() {
   return (
     <div className={sidebarContainer.class}>
       <FeedbackAppName menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <SidebarMenu />
+      <SidebarMenu menuOpen={menuOpen} />
     </div>
   );
 }
