b689dbd311d234ced3c21723f4856f6ba393d137
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-04-27 22:52:13 +0500

    setData on new feedback

--- app/new-feedback/page.tsx
+++ app/new-feedback/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+import initial_data from "@/public/data.json";
 import PlusIcon from "@/public/plus.svg";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import useLocalStorageState from "use-local-storage-state";
 import BackButton from "../components/BackButton";
 import Button from "../components/Button";
 import { Dropdown } from "../components/Dropdown";
@@ -58,9 +64,48 @@ const sectionDesc = tw.style({
   color: "text-gray-dark",
 });
 
+const Categories = ["Feature", "UI", "UX", "Enhancement", "Bug"];
+
 export default function NewFeedback() {
+  const [data, setData] = useLocalStorageState("data", {
+    defaultValue: initial_data,
+  });
+  const [title, setTitle] = useState("");
+  const router = useRouter();
+  const [titleError, setTitleError] = useState<string | undefined>();
+  const titleRef = useRef(null);
+  const [category, setCategory] = useState<string | undefined>(Categories[0]);
+  const [feedbackDetail, setFeedbackDetail] = useState("");
+
+  const AddFeedback = () => {
+    if (title.length < 5) {
+      setTitleError("Title must be at least 5 characters long");
+      /* @ts-ignore */
+      titleRef.current?.focus();
+      return;
+    }
+    data.productRequests.push({
+      id:
+        data.productRequests.length === 0
+          ? 1
+          : data.productRequests.length === 1
+            ? data.productRequests[0].id + 1
+            : data.productRequests.reduce((a, b) => (a.id > b.id ? a : b)).id +
+              1,
+      title: title,
+      category: category ?? "",
+      upvotes: 0,
+      status: "suggestion",
+      description: feedbackDetail,
+      comments: [],
+    });
+    setData(data);
+    toast.success("Feedback added successfully!");
+    // router.back();
+  };
   return (
     <div className={formPage.class}>
+      <Toaster />
       <div className={back.class}>
         <BackButton color="gray">Go Back</BackButton>
       </div>
@@ -72,7 +117,20 @@ export default function NewFeedback() {
           <span className={sectionDesc.class}>
             Add a short, descriptive headline
           </span>
-          <TextField />
+          <TextField
+            ref={titleRef}
+            errorMessage={titleError}
+            value={title}
+            isError={titleError !== undefined}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              if (e.target.value.length < 5) {
+                setTitleError("Title must be at least 5 characters long");
+              } else {
+                setTitleError(undefined);
+              }
+            }}
+          />
         </div>
         <div className={section.class}>
           <span className={sectionTitle.class}>Category</span>
@@ -80,7 +138,13 @@ export default function NewFeedback() {
             Choose a category for your feedback
           </span>
           <div className="relative">
-            <Dropdown options={["Feature", "UI", "UX", "Enhancement", "Bug"]} />
+            <Dropdown
+              onChange={(option) => {
+                setCategory(option);
+              }}
+              options={Categories}
+              defaultOption={Categories[0]}
+            />
           </div>
         </div>
         <div className={`static ${section.class}`}>
@@ -91,15 +155,19 @@ export default function NewFeedback() {
           </span>
           <textarea
             className="h-[120px] resize-none bg-gray  p-4 rounded-lg active:outline-blue focus:outline-blue"
-            name=""
-            id=""
+            value={feedbackDetail}
+            onChange={(e) => setFeedbackDetail(e.target.value)}
             cols={5}
             rows={10}
           />
         </div>
         <div className="flex flex-col gap-[16px] tablet:flex-row-reverse tablet:justify-start">
-          <Button color="purple">Add Feedback</Button>
-          <Button color="dark-blue">Cancel</Button>
+          <Button onClick={AddFeedback} color="purple">
+            Add Feedback
+          </Button>
+          <Button onClick={() => router.back()} color="dark-blue">
+            Cancel
+          </Button>
         </div>
       </div>
     </div>
